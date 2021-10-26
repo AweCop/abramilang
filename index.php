05a38cf61f849fc2d2229dcff26cb4fbca3d8d1c
AweCop <39494571+AweCop@users.noreply.github.com>
2021-10-26 22:13:59 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- MYCSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;500&family=Ubuntu:wght@300;400;700&display=swap" rel="stylesheet">
+    <!--FONT AWESOME -->
+    <script src="https://kit.fontawesome.com/f5279b3413.js" crossorigin="anonymous"></script>
+
+
+    <title>Abrami</title>
+</head>
+<body>
+
+    <div class="header">
+        <h1 class="brand-name">abrami lang</h1>
+
+        <button class="code-btn run-btn">
+            <i class="fas fa-play run"></i>
+        </button>
+        
+        <button class="code-btn abort-btn">
+            <i class="fas fa-stop abort"></i>
+        </button>
+    </div>
+
+
+
+    <div class="text-area" contenteditable="true">
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>    
+</body>
+</html>
